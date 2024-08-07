5dff882278db773eab8a6605317371af57f764a8
aintDatCap <federico.caprini@protonmail.com>
2024-08-07 18:29:12 +0200

    Functions to read data from a wav file

--- src/audio/wav/wav.c
+++ src/audio/wav/wav.c
@@ -1,5 +1,6 @@
 #include "wav.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 wavHeader_t wav_extract_header(FILE *in) {
     rewind(in); // set file pointer to the start of the file
@@ -8,4 +9,21 @@ wavHeader_t wav_extract_header(FILE *in) {
     fread(&wav_header, sizeof(wavHeader_t), 1, in);
 
     return wav_header;
+}
+
+// this loads the whole file into memory,
+// we need a way to divide this into buffers
+uint8_t *wav_read_entire_data_chunk(wavHeader_t wav_header, FILE *in) {
+    uint8_t *data = (uint8_t *)malloc(wav_header.dataSubchunkSize);
+    fread(data, wav_header.dataSubchunkSize, 1, in);
+
+    return data;
+}
+
+// it should ammount to one second of data
+uint8_t *wav_read_next_sample(wavHeader_t wav_header, FILE *in) {
+    uint8_t *data = (uint8_t *)malloc(wav_header.byteRate);
+    fread(data, wav_header.byteRate, 1, in);
+    
+    return data;
 }
