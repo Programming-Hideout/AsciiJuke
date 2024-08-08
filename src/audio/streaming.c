4ae898788d751a8835a5fa593360ad274e6e0a91
aintDatCap <federico.caprini@protonmail.com>
2024-08-08 18:25:07 +0200

    Initialization functions for portaudio

--- src/audio/streaming.c
+++ src/audio/streaming.c
@@ -1,5 +1,15 @@
 #include "streaming.h"
+#include "portaudio.h"
+#include <stdint.h>
 
-void initialize_portaudio() {
-    
+PaError initialize_portaudio() { return Pa_Initialize(); }
+
+static int portaudio_callback(const void *input_buffer, void *output_buffer, unsigned long frames_per_buffer,
+                              const PaStreamCallbackTimeInfo *time_info, PaStreamCallbackFlags status_flags,
+                              void *user_data) {
+
+    uint8_t *data = (uint8_t *)user_data;
+
+    // TODO
+    return 0;
 }
--- src/audio/streaming.h
+++ src/audio/streaming.h
@@ -3,6 +3,10 @@
 #ifndef STREAMING_H
 #define STREAMING_H
 
-void initialize_portaudio();
+PaError initialize_portaudio();
+
+static int portaudio_callback(const void *input_buffer, void *output_buffer, unsigned long frames_per_buffer,
+                              const PaStreamCallbackTimeInfo *time_info, PaStreamCallbackFlags status_flags,
+                              void *user_data);
 
 #endif // STREAMING_H
