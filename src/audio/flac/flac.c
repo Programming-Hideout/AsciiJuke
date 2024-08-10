434b858c1ab47eb1329f090696888b2fd972ca08
Katalysmus <katalysmus@gmail.com>
2024-08-10 12:22:46 +0100

    Continued work on flac.c 2

--- src/audio/flac/flac.c
+++ src/audio/flac/flac.c
@@ -23,9 +23,6 @@ typedef struct audioFrame {
 */
 
 //*
-#include "flac.h"
-#include <stdio.h>
-
 int main(void) {
     flacStream_t ff;
     FILE *in = fopen("./example1.flac", "rb");
@@ -94,13 +91,20 @@ void __flac_blocktype_streaminfo_extract(bt_streamInfo_t *si, void *data) {
     u24Storage >>= 4; // we made it to haskell :cheer:
     ERROR((u24Storage != 0), "invalid sample rate");
     si->sampleRate = u24Storage;
+    printf("%02x %02x %02x %02x\n", data_ptr[byteOffset], data_ptr[byteOffset + 1], data_ptr[byteOffset + 2],
+           data_ptr[byteOffset + 3]);
     byteOffset += 2; // now this is shit. We extract 20 bits, leaving 4
-                     // [bo][bo][bo][bo] [nc][nc][nc] [ps][ps][ps][ps][ps] [_][_][_][_]
+
+    // ([bo][bo][bo][bo] [nc][nc][nc] [ps])([ps][ps][ps][ps] [_][_][_][_])
 
     // FIXME: {{{
     // This code may not work and we may need to run external scripts to verify
-    uint16_t extraction = data_ptr[byteOffset] << 8 | data_ptr[byteOffset + 1];
-    printf("%u \n", extraction >> 8 & 0x7);
+    uint16_t extraction = ((data_ptr[byteOffset] << 8)) | data_ptr[byteOffset + 1];
+    printf("\n%u\n", (extraction >> 1) & ((1 << 5) - 1));
+    for (int i = 0; i <= 16; ++i) {
+        uint16_t e = (extraction >> i) & ((1 << 3) - 1);
+        printf("%u:\t %02x\t%u\t%0b\n", i, e, e, e);
+    }
     si->numChannels = (data_ptr[byteOffset] >> 0) & 0x7;
     si->bitsPerSample = data_ptr[byteOffset] & 0x1f;
     // we are at 12 now bits now so we can increment byteOffset
