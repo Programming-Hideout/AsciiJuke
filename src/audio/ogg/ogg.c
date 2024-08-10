d754cb6b5dad160e7d231b323315c444b5cc0244
kapil <kapil.kumar.baser@gmail.com>
2024-08-10 20:53:07 +0530

    added function to print the page header and calculate the next offset of page header

--- src/audio/ogg/ogg.c
+++ src/audio/ogg/ogg.c
@@ -1,8 +1,15 @@
 #include "ogg.h"
 
-oggHeader_t ogg_extract_header(FILE *in) {
-    oggHeader_t header;
+oggPageHeader_t ogg_extract_header(FILE *in) {
+    oggPageHeader_t header;
     fread(&header, HEADER_SIZE, 1, in);
+    // Print parsed header information
+    print_page_header(&header);
+    // Getting the offset of next oggs page header
+    uint8_t *segmentTable = malloc(header.segments);
+    fread(segmentTable, 1, header.segments, in);
+    uintptr_t nextOffset = (ftell(in) + *segmentTable);
+    // TODO: Getting the size of data
     return header;
 }
 
@@ -26,4 +33,15 @@ static int read_n_bytes(FILE *in, uint8_t *data, int n) {
     else {
         return 0;
     }
+}
+
+void print_page_header(oggPageHeader_t *pageHeader) {
+    printf("Ogg Page Information:\n");
+    printf("Version: %d\n", pageHeader->streamVersion);
+    printf("Header Type: %d\n", pageHeader->typeFlag);
+    printf("Granule Position: %llu\n", (unsigned long long)pageHeader->granulePosition);
+    printf("Serial Number: %u\n", pageHeader->streamID);
+    printf("Sequence Number: %u\n", pageHeader->pageNumber);
+    printf("Checksum: %u\n", pageHeader->checksum);
+    printf("Segments: %d\n", pageHeader->segments);
 }
