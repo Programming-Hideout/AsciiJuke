8690ab27c26f42f1427470feca24299cd4425d78
aintDatCap <federico.caprini@protonmail.com>
2024-08-13 14:56:08 +0200

    Wav datasource

--- src/audio/wav/wav_datasource.h
+++ src/audio/wav/wav_datasource.h
@@ -1,4 +1,11 @@
 #include "miniaudio.h"
+#include "wav.h"
+
+typedef struct
+{
+    ma_data_source_base base;
+    wav_data_t wav_data;
+} wav_data_source_t;
 
 static ma_result wav_data_source_read(ma_data_source *pDataSource, void *pFramesOut, ma_uint64 frameCount,
                                      ma_uint64 *pFramesRead);
@@ -11,3 +18,8 @@ static ma_result wav_data_source_get_data_format(ma_data_source *pDataSource, ma
 static ma_result wav_data_source_get_cursor(ma_data_source *pDataSource, ma_uint64 *pCursor);
 
 static ma_result wav_data_source_get_length(ma_data_source *pDataSource, ma_uint64 *pLength);
+
+static ma_data_source_vtable g_wav_data_source_vtable;
+
+ma_result wav_data_source_init(wav_data_source_t* wav_data_source);
+void wav_data_source_uninit(wav_data_source_t* wav_data_source);
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "audio/wav/wav_datasource.h"
 
 #define MA_NO_VORBIS    /* Disable the built-in Vorbis decoder to ensure the libvorbis decoder is picked. */
 #define MA_NO_OPUS      /* Disable the (not yet implemented) built-in Opus decoder to ensure the libopus decoder is picked. */
