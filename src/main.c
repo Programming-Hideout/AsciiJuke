9fd9c784846def8dbf4b291b33da05e60f2bb6de
aintDatCap <default.pentagon042@passinbox.com>
2024-08-17 17:54:08 +0200

    Removed miniaudio

--- src/main.c
+++ src/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include "audio/wav/wav_datasource.h"
-
-#define MA_NO_VORBIS    /* Disable the built-in Vorbis decoder to ensure the libvorbis decoder is picked. */
-#define MA_NO_OPUS      /* Disable the (not yet implemented) built-in Opus decoder to ensure the libopus decoder is picked. */
-#define MINIAUDIO_IMPLEMENTATION
+#include "ncurses/ncurses.h"
 
 int main() { 
     printf("Hello world\n\n");
